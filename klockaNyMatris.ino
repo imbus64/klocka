02b63bd63914a6c7efa4f8142615bb7e5ea169bd
William Andersson <62219415+WilliamAnderssonDev@users.noreply.github.com>
2023-09-11 20:07:32 +0200

    Update klockaNyMatris.ino

--- klockaNyMatris.ino
+++ klockaNyMatris.ino
@@ -1,63 +1,77 @@
 /*
-Kod för Klocka NY MATRIS
+!!! Endast kod för klocka med SK6812WWA leds och med nya matrisen !!!
 Hårdvara: Melvin Olsson | Mjukvara: William Andersson
-Senast ändrad: 2023-09-07 22:10
+Version: 1
 */
+//Libraries
 #include <Arduino.h>
-#include <Wire.h>     // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
-#include "RTClib.h"   //RTC lib Logic & functions Neiron (Inte Adafruit)
-#include <FastLED.h> //lib funktions for ws2812b
-#include <OneButton.h> //till knappen
+#include <Wire.h>
+#include <RTClib.h>   //by Neiron, version 1.6.3
+#include <FastLED.h> //by Daniel Garcia, version 3.6.0
+#include <OneButton.h> //by Matthias Hertel, version 2.1.0
 
 //LED inställningar
-#define LED_PIN 2
-#define NUM_LEDS 152 //inklusive minut
-CRGB ledColors[8];
-int ledColorsSize = ((sizeof(ledColors) / sizeof(ledColors[0])) - 1);
-int currentColor = constrain(1, 0, ledColorsSize);
+const int LED_PIN = 2; //Pin på arduino
+const int NUM_LEDS = 152; //Hur många leds som finns(inkl minut)
 
-int minut1LedNr = 151;
-int minut2LedNr = 150;
-int minut3LedNr = 149;
-int minut4LedNr = 148;
+//Vilken led som de olika minut leds är
+const int minutLedNr1 = 151; //Minut 1
+const int minutLedNr2 = 150; //Minut 2
+const int minutLedNr3 = 149; //Minut 3
+const int minutLedNr4 = 148; //Minut 4
 
-//Knapp inställningar
-#define BUTTON_PIN 5
+//Olika färglägen.
+const CRGB colors[] = {
+  //Amber | Kall vit | Varm vit
+  CRGB(0,255,0),
+  CRGB(0,255,50),
+  CRGB(0,200,100),
+  CRGB(0,150,150),
+  CRGB(0,100,200),
+  CRGB(0,50,255),
+  CRGB(0,0,255),
+  CRGB(255,0,255),
+};
+int colorsSize = ((sizeof(colors) / sizeof(colors[0])) - 1);
+int currentColor = constrain(1, 0, colorsSize);
+int brightness = constrain(50, 5, 255); // 180
 
+//Inställningar för knappen
+const int BUTTON_PIN = 5; //Pin på arduino
 OneButton btn = OneButton(
   BUTTON_PIN,  // Input pin for the button
   true,        // Button is active LOW
   true         // Enable internal pull-up resistor
 );
 
-//Program Variablar
-CRGB leds[NUM_LEDS];
-DS1307 rtc;
-const int X = 199; //Placeholder led
-DateTime pastTime = DateTime(0,0,0,0,0,0); // Sätter pastTime på noll som start.
-
-int brightness = constrain(180, 5, 255);
 bool btnLongPress = false;
 bool brightnessUpDown = true; // true = upp
 int btnHoldCount = 0;
 
+//Program variablar
+const int X = 199; //Placeholder led
+CRGB leds[NUM_LEDS];
+DS1307 rtc;
+DateTime pastTime = DateTime(0,0,0,0,0,0); // Sätter pastTime på noll som start.
+
+//LED MATRIS
 const int hourLeds[12][8] //ETT till TOLV
 {
-  /*0 ETT*/ {10,11,28,29,50,51, X,X},
-  /*1 TVÅ*/ {12,13,26,27,52,53, X,X},
-  /*2 TRE*/ {14,15,24,25,54,55, X,X},
-  /*3 FYRA*/ {92,93,114,115,128,129,140,141},
-  /*4 FEM*/ {112,113,130,131,138,139, X,X},
-  /*5 SEX*/ {16,17,22,23,56,57, X,X},
-  /*6 SJU*/ {116,117,126,127,142,143, X,X},
-  /*7 ÅTTA*/ {62,63,78,79,84,85,86,87},
-  /*8 NIO*/ {18,19,20,21,58,59, X,X},
-  /*9 TIO*/ {118,119,124,125,144,145, X,X},
+  /*0 ETT  */ {10,11,28,29,50,51, X,X},
+  /*1 TVÅ  */ {12,13,26,27,52,53, X,X},
+  /*2 TRE  */ {14,15,24,25,54,55, X,X},
+  /*3 FYRA */ {92,93,114,115,128,129,140,141},
+  /*4 FEM  */ {112,113,130,131,138,139, X,X},
+  /*5 SEX  */ {16,17,22,23,56,57, X,X},
+  /*6 SJU  */ {116,117,126,127,142,143, X,X},
+  /*7 ÅTTA */ {62,63,78,79,84,85,86,87},
+  /*8 NIO  */ {18,19,20,21,58,59, X,X},
+  /*9 TIO  */ {118,119,124,125,144,145, X,X},
   /*10 ELVA*/ {60,61,80,81,82,83,88,89},
   /*11 TOLV*/ {90,91,120,121,122,213,146,147},
 };
 
-const int wordLeds[12][22] //Över, Halv osv..
+const int wordLeds[12][22] //ÖVER, HALV, osv..
 {
   /*0 Klockan Är   */ {0,1,38,39,40,41,70,71,72,73,96,97,98,99,104,105,134,135, X,X,X,X},
   /*1 Fem I        */ {2,3,36,37,42,43,108,109, X,X,X,X,X,X,X,X,X,X,X,X,X,X},
@@ -75,48 +89,46 @@ const int wordLeds[12][22] //Över, Halv osv..
 
 const int minuteLeds[4][4] //Små lamporna som visar minut
 {
-  /*Minut 1*/ {minut1LedNr, X,X,X},
-  /*Minut 2*/ {minut1LedNr,minut2LedNr, X,X},
-  /*Minut 3*/ {minut1LedNr,minut2LedNr,minut3LedNr, X},
-  /*Minut 4*/ {minut1LedNr,minut2LedNr,minut3LedNr,minut4LedNr},
+  /*Minut 1*/ {minutLedNr1, X,X,X},
+  /*Minut 2*/ {minutLedNr1,minutLedNr2, X,X},
+  /*Minut 3*/ {minutLedNr1,minutLedNr2,minutLedNr3, X},
+  /*Minut 4*/ {minutLedNr1,minutLedNr2,minutLedNr3,minutLedNr4},
 };
 
-//FUNKTIONER
+
+//ARDUINO FUNKTIONER
 void setup()
 {
+  Serial.begin(9600);
+  //Stara nödvändiga libs
   Wire.begin();
+  delay(50);
   rtc.begin();
-  if (!rtc.begin()){} // Om arduino inte kan hitta RTC.
-  if (!rtc.isrunning()) {}
-
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //Uppdatera tid till tiden då senaste gången koden laddades upp | Ladda först upp med det på för att sätta tiden, sen kommentera bort det och laddaa upp igen.
-
-  CRGB colors[] = {
-    //Amber | Kall vit | Varm vit
-    CRGB(0,255,0),
-    CRGB(0,255,50),
-    CRGB(0,200,100),
-    CRGB(0,150,150),
-    CRGB(0,100,200),
-    CRGB(0,50,255),
-    CRGB(0,0,255),
-    CRGB(255,0,255),
-  };
-  for (int i = 0; i < (ledColorsSize + 1); i++)
-  {
-    ledColors[i] = colors[i];
+  while (!rtc.isrunning()) {
+    // Väntar tills RTC är igång
+    Serial.println("rtc not avalible");
+    delay(100);
   }
+  delay(25);
+  //Allt nödvändigt är igång
+
+  //Uppdatera tid till tiden då senaste gången koden laddades upp.
+  //Ladda först upp med det på för att sätta tiden, sen kommentera bort det och ladda upp igen.
+  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(brightness); // Set initial brightness
-  LedsOFF();
-  FastLED.show();
+  delay(25);
 
+  ClockStart();
+  delay(50);
   btn.attachClick(btnClick);
   btn.attachLongPressStart(btnLongPressStart);
   btn.attachLongPressStop(btnLongPressStop);
+  delay(25);
 }
 
+//FUNKTIONER
 void LedsOFF()
 {
   for(int led = 0; led < NUM_LEDS; led++)
@@ -124,6 +136,24 @@ void LedsOFF()
     leds[led].setRGB(0,0,0);
   }
   FastLED.show();
+  delay(25);
+}
+
+void ClockStart()
+{
+  LedsOFF();
+  delay(25);
+  for(int led = 0; led < (floor((NUM_LEDS / 2)) + 10); led++)
+  {
+    leds[led] = colors[currentColor];
+    leds[(NUM_LEDS - led)] = colors[currentColor];
+    delay(5);
+    FastLED.show();
+    delay(25);
+  }
+  delay(50);
+  LedsOFF();
+  delay(25);
 }
 
 void ChangeTime(int _hour, int _minute)
@@ -139,11 +169,12 @@ void ChangeTime(int _hour, int _minute)
   int wordLedsCols = (sizeof(wordLeds[0]) / sizeof(wordLeds[0][0]));
   int hourLedsCols = (sizeof(hourLeds[0]) / sizeof(hourLeds[0][0]));
   int minuteLedsCols = (sizeof(minuteLeds[0]) / sizeof(minuteLeds[0][0]));
+  delay(25);
 
   //Tänd Klockan är
   for(int i = 0; i < wordLedsCols; i++)
   {
-    leds[wordLeds[0][i]] = ledColors[currentColor];
+    leds[wordLeds[0][i]] = colors[currentColor];
   }
   delay(25);
 
@@ -172,9 +203,11 @@ void ChangeTime(int _hour, int _minute)
     _hour = 0;
   }
 
+  delay(25);
+
   for(int i = 0; i < hourLedsCols; i++)
   {
-    leds[hourLeds[_hour][i]] = ledColors[currentColor];
+    leds[hourLeds[_hour][i]] = colors[currentColor];
   }
   delay(25);
 
@@ -194,9 +227,11 @@ void ChangeTime(int _hour, int _minute)
   if(_minute >= 50 && _minute < 55) { minuteRounded = 50; minuteArrayNR = 2; } // XX:50
   if(_minute >= 55) { minuteRounded = 55; minuteArrayNR = 1; } //-----------------XX:55
   
+  delay(25);
+
   for(int i = 0; i < wordLedsCols; i++)
   {
-    leds[wordLeds[minuteArrayNR][i]] = ledColors[currentColor];
+    leds[wordLeds[minuteArrayNR][i]] = colors[currentColor];
   }
   delay(25);
 
@@ -205,7 +240,7 @@ void ChangeTime(int _hour, int _minute)
   {
     for(int i = 0; i < minuteLedsCols; i++)
     {
-      leds[minuteLeds[(_minute - 1)][i]] = ledColors[currentColor];
+      leds[minuteLeds[(_minute - 1)][i]] = colors[currentColor];
     }
   }
   else if(_minute > 5)
@@ -217,42 +252,50 @@ void ChangeTime(int _hour, int _minute)
     {
       for(int i = 0; i < minuteLedsCols; i++)
       {
-        leds[minuteLeds[displayMin][i]] = ledColors[currentColor];
+        leds[minuteLeds[displayMin][i]] = colors[currentColor];
       }
     }
   }
   else
   {
-    leds[minut1LedNr].setRGB(0,0,0);
-    leds[minut2LedNr].setRGB(0,0,0);
-    leds[minut3LedNr].setRGB(0,0,0);
-    leds[minut4LedNr].setRGB(0,0,0);
+    leds[minutLedNr1].setRGB(0,0,0);
+    leds[minutLedNr2].setRGB(0,0,0);
+    leds[minutLedNr3].setRGB(0,0,0);
+    leds[minutLedNr4].setRGB(0,0,0);
   }
-  delay(25);
+  delay(50);
 
   //Uppdatera allt
   FastLED.setBrightness(brightness);
   FastLED.show();
 }
 
+bool isSummerTime(DateTime now) {
+  // Den första dagen för sommartid.
+  DateTime startDST(now.year(), 3, 31 - (5 + now.year() * 5 / 4) % 7, 2, 0, 0); // Sista söndagen i mars, 02:00
+  // Den sista dagen för sommartid.
+  DateTime endDST(now.year(), 10, 31 - (2 + now.year() * 5 / 4) % 7, 3, 0, 0);  // Sista söndagen i oktober, 03:00
+
+  return (now >= startDST && now < endDST);
+}
+
 void btnClick()
 {
   DateTime rtcTime = rtc.now();
-  if(currentColor == ledColorsSize)
+  if(currentColor == colorsSize)
   {
     currentColor = 0;
     ChangeTime(rtcTime.hour(), rtcTime.minute());
   }
   else
   {
-    currentColor = constrain((currentColor + 1), 0, ledColorsSize);
+    currentColor = constrain((currentColor + 1), 0, colorsSize);
     ChangeTime(rtcTime.hour(), rtcTime.minute());
   }
 }
 
 void btnLongPressStart()
 {
-
   if(brightnessUpDown == true) {  brightnessUpDown = false;  }
   else if(brightnessUpDown == false) {  brightnessUpDown = true;  }
 
@@ -262,15 +305,6 @@ void btnLongPressStart()
 void btnLongPressStop()
 {
   btnLongPress = false;
-}
-
-bool isSummerTime(DateTime now) {
-    // Den första dagen för sommartid.
-    DateTime startDST(now.year(), 3, 31 - (5 + now.year() * 5 / 4) % 7, 2, 0, 0); // Sista söndagen i mars, 02:00
-    // Den sista dagen för sommartid.
-    DateTime endDST(now.year(), 10, 31 - (2 + now.year() * 5 / 4) % 7, 3, 0, 0);  // Sista söndagen i oktober, 03:00
-
-    return (now >= startDST && now < endDST);
 }
 
 void loop()
@@ -310,3 +344,5 @@ void loop()
     }
   }
 }
+
+
